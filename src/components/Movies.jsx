c5d168247defac6b7162da54009aba28b9300aef
lucky favour <luckyfavour1219@gmail.com>
2023-10-26 12:59:26 -0700

    commit

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -5,10 +5,23 @@ import { getMovies } from '../services/fakeMovieService';
 class Movies extends Component {
     state = { 
         movies: getMovies()
-     } 
+     };
+
+     handleDelete = (movie) => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies})
+     }
 
     render() { 
+        const {length: count} = this.state.movies;
+
+        if (count === 0) return <p>There are no movies in the database</p>
+
+
         return (
+            <>
+            
+            <p>Showing {count} {count ==1 ? 'movie' : 'movies'} in the database </p>
             <table className="table">
                 <thead>
                     <tr>
@@ -16,19 +29,22 @@ class Movies extends Component {
                         <th>Genre</th>
                         <th>Stock</th>
                         <th>Rate</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {this.state.movies.map(movie => (
-                    <tr>
+                    <tr key={movie._id}>
                         <td>{movie.title}</td>
                         <td>{movie.genre.name}</td>
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
+                        <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                     </tr>
                     ))}
                 </tbody>
             </table>
+            </>
         );
     }
 }
